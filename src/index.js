65292e45d3216a8d0987122988e0da92b49256be
Uttam Dutta <uttamdutta7272@gmail.com>
2024-05-06 18:37:29 +0530

    api error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv'
 import { connectDB } from './db/index.js'
+import app from './app.js'
 
 dotenv.config({
   path: '.env',
 })
 
+const PORT = process.env.PORT || 8000
 connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Server is running at port0', PORT)
+    })
+    app.on('error', (error) => {
+      console.log('Server ERROR: ', error)
+    })
+  })
+  .catch((error) => {
+    console.log('Server ERROR: ', error)
+  })
